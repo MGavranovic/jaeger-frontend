e3265a16303f7f6be86bab34abeb1bb5a6e8f19b
MGavranovic <mgavranovic7@gmail.com>
2024-10-31 14:58:45 +0100

    Added PageNotFound page to the router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Notes from "./pages/Notes";
+import PageNotFound from "./pages/PageNotFound";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       { path: "/dashboard", element: <Dashboard /> },
       { path: "/notes", element: <Notes /> },
     ],
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
   },
 ]);
 
