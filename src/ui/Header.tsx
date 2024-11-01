84f3aa6d310e56726e1a230e026b9bad132d4db2
MGavranovic <mgavranovic7@gmail.com>
2024-11-01 11:42:55 +0100

    Styled header and added placeholders

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,15 +1,24 @@
+import { HiArrowRightStartOnRectangle } from "react-icons/hi2";
+import { RxAvatar } from "react-icons/rx";
 import styled from "styled-components";
 
 const SyledHeader = styled.header`
-  border-bottom: 1px solid;
+  border-bottom: 1px solid var(--color-zinc-100);
   padding: 3rem;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  gap: 1rem;
 `;
 
 function Header() {
-  return <SyledHeader>HEADER</SyledHeader>;
+  return (
+    <SyledHeader>
+      <span>HEADER</span>
+      <RxAvatar />
+      <HiArrowRightStartOnRectangle />
+    </SyledHeader>
+  );
 }
 
 export default Header;
