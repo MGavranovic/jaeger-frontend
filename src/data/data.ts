400f148690fad8ec7690b7ca765d719fb2ed715c
MGavranovic <mgavranovic7@gmail.com>
2024-11-06 16:56:49 +0100

    Added comments as a reminder xD

---
+++ src/data/data.ts
@@ -0,0 +1,5 @@
+// TODO:
+/*
+Adding this commnet for myself not to forget an idea that came to my mind.
+As I'll need some dummy data, that will be annoying to write by myself, I will have to use faker.js
+*/
