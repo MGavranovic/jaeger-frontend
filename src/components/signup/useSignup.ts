0942497c963af033a5faee2493d19d1a269e3935
MGavranovic <mgavranovic7@gmail.com>
2025-01-02 10:03:40 +0100

    On signup navigate to dashboard

--- src/components/signup/useSignup.ts
+++ src/components/signup/useSignup.ts
@@ -1,12 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
 import { signup as signupApi } from "../../services/apiAuth";
 
 export function useSignup() {
+  const navigate = useNavigate();
+
   const { mutate: signup, isPending } = useMutation({
     mutationFn: signupApi,
     onSuccess: (newUser) => {
       console.log("New user successfully created");
       console.log(newUser);
+      // sending the user data
+      navigate("/dashboard");
     },
     onError: (error) => {
       console.error(error);
