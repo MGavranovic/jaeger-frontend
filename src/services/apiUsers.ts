24ca6fc1f84c4b70d4208c536875db3ae6892f98
MGavranovic <mgavranovic7@gmail.com>
2025-02-12 08:38:48 +0100

    Add fetch to send json for new user data to server

--- src/services/apiUsers.ts
+++ src/services/apiUsers.ts
@@ -6,11 +6,22 @@ export async function getUsers() {
   console.log(data);
 }
 
-export function changeUserSettings({
+export async function changeUserSettings({
   id,
   fullName,
   email,
   password,
 }: UserData) {
   console.log("func changeUserSettings", id, fullName, email, password);
+
+  const res = await fetch("http://localhost:8080/api/users/current/update", {
+    method: "POST",
+    body: JSON.stringify({ id, fullName, email, password }),
+  });
+
+  if (!res.ok) {
+    console.error("HTTP errro!", res.status);
+  } else {
+    console.log("Data for updating sent to server!");
+  }
 }
